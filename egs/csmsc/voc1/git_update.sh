6202800b3bfd74cf0bbd9b28f10d2ec04e73b099
wang xintong <walston874848612@163.com>
2024-06-03 02:26:56 +0000

    multiband istft 16k basemodel Version 1.2

--- egs/csmsc/voc1/git_update.sh
+++ egs/csmsc/voc1/git_update.sh
@@ -2,7 +2,7 @@ git add /home/xintong/ParallelWaveGAN
 
 git commit -m "multiband istft 16k basemodel Version 1.2"
 
-git tag -a v1.2 -m "verison 1.2 of the model"
+# git tag -a v1.2 -m "verison 1.2 of the model"
 
 git push origin v1.2
 git push origin master
